216845be17117f05607f1e45bf7404a08defd1e7
Héctor Acosta Campus Dual <112854864+HectorjagCD@users.noreply.github.com>
2022-09-12 14:51:21 +0200

    test github Desktop working
    Little test in main to verify Github Desktop is working accordingly

--- src/es/freecodecamp/java/ScannerEx.java
+++ src/es/freecodecamp/java/ScannerEx.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  */
 public class ScannerEx {
 
-	//init
+	//Tracking changes in GitHub Desktop
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Tell me what name i'm thinking of?");
